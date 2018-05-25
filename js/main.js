ad60f87988e467381499ac1b39eacc81bc908c7f
Steven Huang <steven.charles.huang@gmail.com>
2018-05-25 01:22:43 -0400

    continued work

--- js/main.js
+++ js/main.js
@@ -20,63 +20,24 @@ $(function() {
   let navPos = $('.nav')[0].getBoundingClientRect();
   let headerPos = $('.header')[0].getBoundingClientRect();
   console.log('.headerPos is:', headerPos)
-  // window.onscroll = function() {scrollFunction()};
-  // Nav Scroller
-  // $('.nav').css({
-  //   'position': 'absolute',
-  //   'bottom': '0px',
-  // });
-  // $(window).scroll(() => {
-  //   ($('.nav').scrollTop(700))
-  //   console.log('navPos is:', navPos);
-  // });
-  // function scrollFunction() {
-  //   console.log('scrollTop: ', document.body.scrollTop)
-  //   if (document.body.scrollTop < headerPos.height) {
-  //       $('navbar').css({
-  //         'position': 'absolute;',
-  //         'bottom': $(window).scrollTop() + "px"
-  //       });
-  //   } else {
-  //       $('navbar').css({'bottom': '0'});
-  //   }
-  // }
-  // When the user scrolls the page, execute myFunction
-  // window.onscroll = function() {myFunction()};
 
-  // // Get the navbar
-  // let navbar = $('.navbar');
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 100) {
+      $('.nav').toggleClass('.fixed');
+    }
+  })
 
-  // // Get the offset position of the navbar
-  // let sticky = navbar.offsetBottom;
+  // Footer Icon Replacement
+  $('#git-link').on('mouseenter', (e) => {
+    $('#git-link').append('&nbsp<span id="git-text">Git</span>');
+    e.stopPropogation();
+  })
+  $('#git-link').on('mouseleave', () => {
+    $('#git-link').remove('#git-text')
+  })
+  // $('#git-link').on('mouseleave', () => {
+  //   $('#git').replaceWith('<img id="git-icon" src="./img/github.png" alt="git">')
+  // })
 
-  // // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  // function myFunction() {
-  //   if (window.pageYOffset >= sticky) {
-  //     navbar.classList.add("sticky")
-  //   } else {
-  //     navbar.classList.remove("sticky");
-  //   }
-  // }
-  let stickyNavTop = $('.nav').offset().bottom;
-
-        // our function that decides weather the navigation bar should have "fixed" css position or not.
-        let stickyNav = function(){
-          let scrollTop = $(window).scrollTop(); // our current vertical position from the top
-
-          // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-          // otherwise change it back to relative
-          if (scrollTop > stickyNavTop) {
-              $('.nav').addClass('sticky');
-          } else {
-              $('.nav').removeClass('sticky');
-          }
-      };
-
-      stickyNav();
-      // and run it again every time you scroll
-      $(window).scroll(function() {
-        stickyNav();
-      });
 // Closes off jQuery Load-Check
 });
