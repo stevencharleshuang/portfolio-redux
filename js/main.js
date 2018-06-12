f22fdc2468a04a815efe9d8aacdb8032338f958c
stevencharleshuang <steven.charles.huang@gmail.com>
2018-06-12 00:30:49 -0400

    Added landing page content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(function() {
+  /*
+                               __        __                      __        __          __  __  __ 
+                            /  |      /  |                    /  |      /  |        /  |/  |/  |
+    ______   __    __   ____$$ |      $$ |  ______    ______  $$ |   __ $$/_______  $$ |$$ |$$ |
+   /      \ /  |  /  | /    $$ |      $$ | /      \  /      \ $$ |  /  |$//       \ $$ |$$ |$$ |
+  /$$$$$$  |$$ |  $$ |/$$$$$$$ |      $$ |/$$$$$$  |/$$$$$$  |$$ |_/$$/   $$$$$$$  |$$ |$$ |$$ |
+  $$ |  $$ |$$ |  $$ |$$ |  $$ |      $$ |$$ |  $$ |$$ |  $$ |$$   $$<    $$ |  $$ |$$/ $$/ $$/ 
+  $$ \__$$ |$$ \__$$ |$$ \__$$ |      $$ |$$ \__$$ |$$ \__$$ |$$$$$$  \   $$ |  $$ | __  __  __ 
+  $$    $$ |$$    $$/ $$    $$ |      $$ |$$    $$/ $$    $$/ $$ | $$  |  $$ |  $$ |/  |/  |/  |
+   $$$$$$$ | $$$$$$/   $$$$$$$/       $$/  $$$$$$/   $$$$$$/  $$/   $$/   $$/   $$/ $$/ $$/ $$/ 
+  /  \__$$ |                                                                                    
+  $$    $$/                                                                                     
+   $$$$$$/       
+
+  */
   console.log( "jQuery ready and happy!" );
 
   const landing = $('.landing');
@@ -72,6 +87,32 @@ $(function() {
     $('#stuff').text('Stuff')
   });
 
+  // https://codepen.io/cooljockey/pen/uyeHz
+  animateDiv('.a');
+  animateDiv('.b');
+  animateDiv('.c');
+  animateDiv('.d');
+
+  function makeNewPosition(){
+      
+      // Get viewport dimensions (remove the dimension of the div)
+      var h = $(window).height() - 200;
+      var w = $(window).width() - 200;
+      
+      var nh = Math.floor(Math.random() * h);
+      var nw = Math.floor(Math.random() * w);
+      
+      return [nh,nw];    
+      
+  }
+
+  function animateDiv(myclass){
+      var newq = makeNewPosition();
+      $(myclass).animate({ top: newq[0], left: newq[1] }, 2000,   function(){
+        animateDiv(myclass);        
+      });
+      
+  };
 
 // Closes off jQuery Load-Check
 });
